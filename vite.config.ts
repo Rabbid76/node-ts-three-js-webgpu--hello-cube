5fbfad91e674ba60c5dac58e87108286062e9b2a
Gernot Steinegger <Gernot.Steinegger@roomle.com>
2025-03-02 19:09:46 +0100

    fix: vite - relative asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  base: './',
   build: {
-    outDir: '../dist'
-  }
-})
+    outDir: '../dist',
+  },
+});
